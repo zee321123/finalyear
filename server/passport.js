3089be8a9e677a7a70cb48605792704fb13fdb66
zee321123 <zaidbathspa.ae@gmail.com>
2025-05-18 13:02:25 -0700

    update

--- server/passport.js
+++ server/passport.js
@@ -5,7 +5,8 @@ const User = require('./models/user');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+
 }, async (accessToken, refreshToken, profile, done) => {
   // Find or create user
   let user = await User.findOne({ email: profile.emails[0].value });
